976a123c5e5f67c8abe4e71597c3b347eb221782
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-04-26 21:48:45 +0530

    Create a responsive navbar

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import router from './routers/router.jsx'
 import { RouterProvider } from 'react-router-dom'
@@ -6,8 +5,9 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-    <App />
-  </StrictMode>,
+
+    <RouterProvider router={router}>
+        <App />
+    </RouterProvider>
+
 )
